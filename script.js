e8643bc5992c4e5ab28fbff988e73f517105e551
Juliana <jumiranda0501@gmail.com>
2023-01-24 16:53:43 -0300

    Initial js logic

---
+++ script.js
@@ -0,0 +1,21 @@
+const Player = () => {
+
+}
+
+const gameBoard = (() => {
+
+    const board = [
+        "", "", "", 
+        "", "", "", 
+        "", "", ""
+    ];
+
+    const addMovement = (symbol, position) => {};
+
+})();
+
+// play against human or bot
+// choose symbol (x will always start)
+// add players movements
+// show winner
+// button restart

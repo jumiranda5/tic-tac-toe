e85d92497cfdf9bb52495f247decf8ebe5c7b04f
Juliana <jumiranda0501@gmail.com>
2023-02-02 16:16:13 -0300

    UI styles

--- script.js
+++ script.js
@@ -14,8 +14,37 @@ const gameBoard = (() => {
 
 })();
 
-// play against human or bot
-// choose symbol (x will always start)
-// add players movements
-// show winner
-// button restart
+
+// UI
+
+const settingsVs = document.getElementById("settings-vs");
+const settingsSymbol = document.getElementById("settings-symbol");
+const boardContainer = document.getElementById("board-container");
+const btnPlayer2 = document.getElementById("btn-player2");
+const btnBot = document.getElementById("btn-bot");
+const btnX = document.getElementById("btn-x");
+const btnO = document.getElementById("btn-o");
+
+btnPlayer2.addEventListener("click", () => {
+    console.log("Play against Player 2");
+    settingsVs.classList.add("hidden");
+    settingsSymbol.classList.remove("hidden");
+});
+
+btnBot.addEventListener("click", () => {
+    console.log("Play against bot");
+    settingsVs.classList.add("hidden");
+    settingsSymbol.classList.remove("hidden");
+});
+
+btnX.addEventListener("click", () => {
+    console.log("Player 1 is X");
+    settingsSymbol.classList.add("hidden");
+    boardContainer.classList.remove("hidden");
+});
+
+btnO.addEventListener("click", () => {
+    console.log("Player 1 is O");
+    settingsSymbol.classList.add("hidden");
+    boardContainer.classList.remove("hidden");
+});

ebba2d1382da5b515f11cde9186d4c09e4295537
Juliana <jumiranda0501@gmail.com>
2023-02-03 08:33:12 -0300

    Add player

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const Player = () => {
-
+const Player = (name, symbol) => {
+    let isBot = false;
+    if (name.toLowerCase() === "bot") isBot = true;
+    return {name, symbol, isBot};
 }
 
 const gameBoard = (() => {
+
+    let player1Name;
+    let player2Name;
+    let player1;
+    let player2;
+
+    const setPlayersNames = (name1, name2) => { 
+        player1Name = name1;
+        player2Name = name2;
+    };
+
+    const setPlayers = (symbol1, symbol2) => {
+        player1 = Player(player1Name, symbol1);
+        player2 = Player(player2Name, symbol2);
+        console.log(player1);
+        console.log(player2);
+    };
 
     const board = [
         "", "", "", 
@@ -10,7 +29,9 @@ const gameBoard = (() => {
         "", "", ""
     ];
 
-    const addMovement = (symbol, position) => {};
+    const addMove = (symbol, position) => {};
+
+    return { setPlayersNames, setPlayers }
 
 })();
 
@@ -29,22 +50,26 @@ btnPlayer2.addEventListener("click", () => {
     console.log("Play against Player 2");
     settingsVs.classList.add("hidden");
     settingsSymbol.classList.remove("hidden");
+    gameBoard.setPlayersNames("Player 1", "Player 2");
 });
 
 btnBot.addEventListener("click", () => {
     console.log("Play against bot");
     settingsVs.classList.add("hidden");
     settingsSymbol.classList.remove("hidden");
+    gameBoard.setPlayersNames("Player 1", "Bot");
 });
 
 btnX.addEventListener("click", () => {
     console.log("Player 1 is X");
     settingsSymbol.classList.add("hidden");
     boardContainer.classList.remove("hidden");
+    gameBoard.setPlayers("x", "o");
 });
 
 btnO.addEventListener("click", () => {
     console.log("Player 1 is O");
     settingsSymbol.classList.add("hidden");
     boardContainer.classList.remove("hidden");
+    gameBoard.setPlayers("o", "x");
 });
